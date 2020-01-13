979623cf9e8a7ef482710b56a6d1fcc78ccc37ce
Laura Berge <lauraeberge@gmail.com>
2020-01-13 14:07:53 -0600

    Done.

--- src/App.js
+++ src/App.js
@@ -1,1 +1,32 @@
-// create your App component here
+import React, {Component} from 'react';
+import { runInThisContext } from 'vm';
+
+export default class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            people: []
+        }
+    }
+
+    render() {
+        return(
+            <div>{this.state.people.map(p => <div>{p.name + "-" + p.craft}</div>)}</div>
+        )
+    }
+
+    componentDidMount() {
+        fetch("http://api.open-notify.org/astros.json")
+            .then(resp => resp.json())
+            .then(obj => {
+                console.log(obj)
+                this.setState({
+                    people: obj.people
+                })
+            })
+            .catch(error => {
+                alert("Error fetching data.");
+                console.log(error.message);
+            })
+    }
+}
